02bf5a940b40a6dc9370e4ece2cdf262f8de4816
VVSaltykov <saltyckov.vitya@yandex.ru>
2023-07-10 15:56:35 +0300

    add html

---
+++ Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		StreamWriter streamwriter = new StreamWriter(@"D:\index.html");
+		streamwriter.WriteLine("<html>");
+		streamwriter.WriteLine("<head>");
+		streamwriter.WriteLine("  <title>HTML-Document</title>");
+		streamwriter.WriteLine("  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+		streamwriter.WriteLine("</head>");
+		streamwriter.WriteLine("<body>");
+		streamwriter.WriteLine("Привет");
+		streamwriter.WriteLine("</body>");
+		streamwriter.WriteLine("</html>");
+		streamwriter.Close();
+	}
+}
